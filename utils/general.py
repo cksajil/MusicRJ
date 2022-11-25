2275e7cc2ba00524cd67a472137910b896c1690c
cksajil <sajildcb@gmail.com>
2022-11-25 06:55:27 +0530

    Created utils directory for generic functions  On branch master  Changes to be committed: 	new file:   utils/__init__.py 	new file:   utils/general.py

---
+++ utils/general.py
@@ -0,0 +1,11 @@
+def strided_app(a, L, S):  
+    """
+    Function to split audio as overlapping segments
+    L : Window length
+    S : Stride (L/stepsize)
+    """
+    nrows = ((a.size-L)//S)+1
+    n = a.strides[0]
+    return np.lib.stride_tricks.as_strided(a,\
+        shape=(nrows,L),\
+        strides=(S*n,n))
