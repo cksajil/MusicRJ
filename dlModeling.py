a83ec6e15ec320bd45c6a4c2a707af3b06fafdb9
cksajil <sajildcb@gmail.com>
2022-11-26 18:11:47 +0530

    Fix styling issues for dlModeling.py  On branch master  Changes to be committed: 	modified:   dlModeling.py

--- dlModeling.py
+++ dlModeling.py
@@ -6,7 +6,6 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from plotter.plots import qualityLinePlot
 from utils.general import create_model
 
-
 # Import refined dataSet
 data_src = 'MasterData.csv'
 essentialdata = pd.read_csv('./Data/'+data_src, low_memory=False).dropna()
@@ -20,9 +19,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     features, labels, test_size=0.2, shuffle=True
 )
 
-# Define the Neural Network Model and return it
-
-
 # Create a basic model instance
 model = create_model()
 
@@ -31,17 +27,14 @@ checkpoint_path = "./TrainedModel/"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 # Create a callback that saves the model's weights
-cp_callback = ModelCheckpoint(filepath=checkpoint_path,\
-	save_weights_only=True,\
-	verbose=1)
+cp_callback = ModelCheckpoint(
+    filepath=checkpoint_path, save_weights_only=True, verbose=1
+)
 
 # Train the model with callback
-history = model.fit(X_train,\
-	y_train,\
-	batch_size = 32,\
-	epochs = 20,\
-	validation_split = 0.20,\
-	callbacks=[cp_callback])
+history = model.fit(
+    X_train, y_train, batch_size=32, epochs=20, validation_split=0.20,
+    callbacks=[cp_callback])
 
 # Load Saved Model
 modeltoDeploy = create_model()
@@ -53,8 +46,9 @@ modeltoDeploy.load_weights(checkpoint_path)
 ndatapoints = features.shape[0]
 
 # Re-evaluate the model
-loss, acc = modeltoDeploy.evaluate(X_test, y_test, verbose=2)
-stats = "Restored model, accuracy: {:5.2f}\% on {} data points".format(100 * acc, ndatapoints)
+loss, test_accuracy = modeltoDeploy.evaluate(X_test, y_test, verbose=2)
+stats = "Restored model, accuracy: {:5.2f}% on {} data points".format(
+    100 * test_accuracy, ndatapoints)	
 print(stats)
 
 # Plot the loss graph
