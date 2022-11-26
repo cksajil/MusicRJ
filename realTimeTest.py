4d429ada069ffa41be363242948f9962acd656f1
cksajil <sajildcb@gmail.com>
2022-11-26 20:09:23 +0530

    Real-time test working ok

--- realTimeTest.py
+++ realTimeTest.py
@@ -1,69 +1,34 @@
 # Import libraries
-import os
 import wave
-import keras
 import pyaudio
-import pandas as pd
 import numpy as np
-import tensorflow as tf
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.initializers import RandomUniform
-
+import pandas as pd
+from utils.general import create_model
+from keras.models import Sequential
+from tensorflow.keras.layers import Softmax
 
 # Set the streaming audio settings
 CHUNK = 256
 WIDTH = 1
 CHANNELS = 1
-RATE = 22050
+RATE = 32000
 RECORD_SECONDS = 30
 
-
-# Define the Neural Network model Used
-def create_model():
-
-    model = Sequential()
-    model.add(Dense(64, activation = 'relu', input_shape= (1, 256)))
-    model.add(Dense(512, kernel_initializer=RandomUniform(minval=-0.05, maxval=0.05),
-        kernel_regularizer=tf.keras.regularizers.l2(0.001),
-        activation=tf.nn.relu))
-    model.add(Dense(128, activation = 'relu', 
-        kernel_regularizer=tf.keras.regularizers.l2(0.001)))
-    model.add(Dense (128, activation = 'relu',
-        kernel_regularizer=tf.keras.regularizers.l2(0.001)))
-    model.add(tf.keras.layers.Dropout(0.5))
-    model.add(Dense (64, activation = 'relu', 
-        kernel_regularizer=tf.keras.regularizers.l2(0.001)))
-    model.add(Dense(2, kernel_initializer=RandomUniform(minval=-0.05, maxval=0.05),
-        kernel_regularizer=tf.keras.regularizers.l2(0.001),
-        activation=tf.nn.softmax))
-
-    model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.0001),
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    return model
-
-
-# Load trained model
-modeltoDeploy = create_model()
-
+# Initialize model
+modeltoDeploy = create_model((CHUNK,))
 
 # Loads the weights
-checkpoint_path = "./TrainedModel/"
-checkpoint_dir = os.path.dirname(checkpoint_path)
-modeltoDeploy.load_weights(checkpoint_path)
-
+file_path = "./TrainedModel/best_model.hdf5"
+modeltoDeploy.load_weights(file_path)
 
 # Create a probability models and labels for prediction
 classes = ['Speech', 'Music']
-probability_model = Sequential([modeltoDeploy, tf.keras.layers.Softmax()])
-
+probability_model = Sequential([modeltoDeploy, Softmax()])
 
 # Load a random file to test
 testfiles = pd.read_csv('./Data/BasicData.csv')['Filename'].values
 testfile = np.random.choice(testfiles)
 wf = wave.open('./Data/Files/'+testfile, 'rb')
-
 
 # Create a PyAudio handle to read, test, and play
 p = pyaudio.PyAudio()
@@ -72,33 +37,28 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 rate=wf.getframerate(),
                 output=True)
 
-
 # Read data
 data = wf.readframes(CHUNK)
 
-
-# Test and play stream 
+# Test and play stream
 freqData = np.empty((0, CHUNK), int)
 k = 0
 while len(data) > 0:
-    k+=1
+    k += 1
     stream.write(data)
-    idata = np.fromstring(data , np.int16)
+    idata = np.fromstring(data, np.int16)
     idata = np.abs(np.fft.fft(idata))
     T = np.sum(idata)/np.max(idata)
-    if T>10:
+    if T > 10:
         row = [list(idata)]
-        if k%10 == 0:
+        if k % 10 == 0:
             try:
-                # pass
                 predictions = probability_model.predict(row)
-                # print(predictions)
                 index = np.argmax(predictions[0])
                 print(classes[index])
             except Exception as e:
                 raise
     data = wf.readframes(CHUNK)
-
 
 # Stop stream once done
 stream.stop_stream()
