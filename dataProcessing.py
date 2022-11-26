189b720eb4ce836a3296c36986f0131a9efb8510
cksajil <sajildcb@gmail.com>
2022-11-26 17:50:19 +0530

    Fixed minor styling issues with dataprocessing.py  On branch master  Changes to be committed: 	modified:   dataProcessing.py

--- dataProcessing.py
+++ dataProcessing.py
@@ -4,7 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 from utils.general import strided_app
 
-basicdata = pd.read_csv('./Data/BasicData.csv').sample(frac=1)
+basicdata = pd.read_csv('./Data/BasicData.csv')
 CorpusPath = './Data/Files/'
 Corpusfiles = basicdata['Filename'].values
 fn = len(Corpusfiles)
@@ -19,18 +19,19 @@ i = 0
 Fcol = []
 
 for file in tqdm(Corpusfiles):
-    i+=1
+    i += 1
     x, Fs = librosa.load(CorpusPath+file)
     segments = strided_app(x, L=frameS, S=offset)
-    for l in range(64):
+    for _ in range(64):
         k = np.random.randint(0, 10332, dtype=int)
         segment = segments[k]
         Fcol.append(file)
         row = np.abs(np.fft.fft(segment))
         spectrumData = np.vstack([spectrumData, row])
 
-spectrumDF = pd.DataFrame(spectrumData, columns = spectrumCol)
+spectrumDF = pd.DataFrame(spectrumData, columns=spectrumCol)
 spectrumDF['Filename'] = Fcol
+
 # Combine basic data with features computed
 masterdata = pd.merge(basicdata, spectrumDF, on='Filename')
 masterdata.to_csv('./Data/MasterData.csv', index=False)
