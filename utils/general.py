c02e60ba54ef0b3bef982b10bc6b9ebed6663ac5
cksajil <sajildcb@gmail.com>
2022-11-26 20:29:18 +0530

    Test Accuracy 99%

--- utils/general.py
+++ utils/general.py
@@ -22,12 +22,20 @@ def strided_app(a, L, S):
 def create_model(in_shape):
     model = Sequential()
 
+    model.add(Dense(32, activation='relu', input_shape=in_shape))
+
     model.add(Dense(64, activation='relu', input_shape=in_shape))
+
+    model.add(Dense(128, kernel_initializer=RandomUniform(minval=-0.05,
+	          maxval=0.05), kernel_regularizer=l2(0.001), activation='relu'))
+
+    model.add(Dense(256, kernel_initializer=RandomUniform(minval=-0.05,
+	          maxval=0.05), kernel_regularizer=l2(0.001), activation='relu'))
 
     model.add(Dense(512, kernel_initializer=RandomUniform(minval=-0.05,
 	          maxval=0.05), kernel_regularizer=l2(0.001), activation='relu'))
 
-    model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.001)))
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.001)))
 
     model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.001)))
 
@@ -38,6 +46,6 @@ def create_model(in_shape):
     model.add(Dense(2, kernel_initializer=RandomUniform(minval=-0.05,
 	          maxval=0.05), kernel_regularizer=l2(0.001), activation='softmax'))
 
-    model.compile(optimizer=RMSprop(learning_rate=0.0001),
+    model.compile(optimizer='Adam',
 	              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
