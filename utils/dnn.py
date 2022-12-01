95200d9a791ed672dd2d4e4a0bd0ce7a2e2d9e6f
cksajil <sajildcb@gmail.com>
2022-12-01 07:32:40 +0530

    Separate Model Code

---
+++ utils/dnn.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from tensorflow.keras.initializers import RandomUniform
+from tensorflow.keras.regularizers import l2
+
+
+def myDNN(in_shape):
+    model = Sequential()
+
+    model.add(Dense(32, activation='relu', input_shape=in_shape))
+
+    model.add(Dense(64, activation='relu', input_shape=in_shape))
+
+    model.add(Dense(128, kernel_initializer=RandomUniform(minval=-0.05,
+                    maxval=0.05), kernel_regularizer=l2(0.001), 
+                    activation='relu'))
+
+    model.add(Dense(256, kernel_initializer=RandomUniform(minval=-0.05,
+                    maxval=0.05), kernel_regularizer=l2(0.001), 
+                    activation='relu'))
+
+    model.add(Dense(512, kernel_initializer=RandomUniform(minval=-0.05,
+                    maxval=0.05), kernel_regularizer=l2(0.001), 
+                    activation='relu'))
+
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.001)))
+
+    model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.001)))
+
+    model.add(tf.keras.layers.Dropout(0.5))
+
+    model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.001)))
+
+    model.add(Dense(2, kernel_initializer=RandomUniform(minval=-0.05,
+                    maxval=0.05), kernel_regularizer=l2(0.001),
+                    activation='softmax'))
+
+    model.compile(optimizer='Adam',
+                  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
--- utils/general.py
+++ utils/general.py
@@ -1,11 +1,7 @@
 import yaml
 import numpy as np
 from os import path
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
-from tensorflow.keras.initializers import RandomUniform
-from tensorflow.keras.regularizers import l2
+from utils.dnn import myDNN
 
 
 def load_config(config_name):
@@ -29,32 +25,5 @@ def strided_app(a, L, S):
 
 
 def create_model(in_shape):
-    model = Sequential()
-
-    model.add(Dense(32, activation='relu', input_shape=in_shape))
-
-    model.add(Dense(64, activation='relu', input_shape=in_shape))
-
-    model.add(Dense(128, kernel_initializer=RandomUniform(minval=-0.05,
-	          maxval=0.05), kernel_regularizer=l2(0.001), activation='relu'))
-
-    model.add(Dense(256, kernel_initializer=RandomUniform(minval=-0.05,
-	          maxval=0.05), kernel_regularizer=l2(0.001), activation='relu'))
-
-    model.add(Dense(512, kernel_initializer=RandomUniform(minval=-0.05,
-	          maxval=0.05), kernel_regularizer=l2(0.001), activation='relu'))
-
-    model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.001)))
-
-    model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.001)))
-
-    model.add(tf.keras.layers.Dropout(0.5))
-
-    model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.001)))
-
-    model.add(Dense(2, kernel_initializer=RandomUniform(minval=-0.05,
-	          maxval=0.05), kernel_regularizer=l2(0.001), activation='softmax'))
-
-    model.compile(optimizer='Adam',
-	              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    return model
+    dnn_model = myDNN(in_shape)
+    return dnn_model
