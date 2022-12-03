93eb87fa1dc52ea388ca5d6d4d2a251bd2b820c9
cksajil <sajilck@gmail.com>
2022-12-03 07:32:45 +0530

    Add CNN Data generating function

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,6 +1,6 @@
 from os import path
 import pandas as pd
-from utils.general import load_config, generateDNNData
+from utils.general import load_config, generateDNNData, generateCNNData
 
 
 def main():
@@ -12,12 +12,8 @@ def main():
     # Segement size and overlapp offset value
     frameS = 256
     offset = 64
-    spectrumDF = generateDNNData(basicdata, frameS=frameS, offset=offset)
-
-    # Combine basic data with features computed
-    masterdata = pd.merge(basicdata, spectrumDF, on='Filename')
-    masterdata.to_csv(path.join(config["data_directory"],
-                                config["master_data"]), index=False)
+    generateCNNData(basicdata, frameS=frameS, offset=offset)
+    generateDNNData(basicdata, frameS=frameS, offset=offset)
 
 
 if __name__ == '__main__':
--- utils/general.py
+++ utils/general.py
@@ -55,8 +55,30 @@ def generateDNNData(basicdata, frameS, offset):
             for segment in segments[ids, :]:
                 Fcol.append(file)
                 row = np.abs(np.fft.fft(segment))
-                spectrumData = np.vstack([spectrumData, row]) 
+                spectrumData = np.vstack([spectrumData, row])
     spectrumDF = pd.DataFrame(spectrumData, columns=spectrumCol)
     spectrumDF['Filename'] = Fcol
 
-    return spectrumDF
+    masterdata = pd.merge(basicdata, spectrumDF, on='Filename')
+    masterdata.to_csv(path.join(config["data_directory"],
+                                config["master_data"]), index=False)
+
+
+def generateCNNData(basicdata, frameS, offset):
+    CorpusPath = path.join(config["data_directory"], config["file_directory"])
+    labels = []
+    for index, row in tqdm(basicdata.iterrows(), total=basicdata.shape[0]):
+        x, Fs = librosa.load(CorpusPath+row['Filename'])
+        spectro_data = np.abs(librosa.stft(x))
+        segments = np.array_split(spectro_data, 10)
+        for segment in segments:
+            labels.append(row['Label'])
+
+    segments = np.concatenate(segments, axis=0)
+    cnn_features_data_path = path.join(config["data_directory"],
+                                       config["cnn_X_data"])
+    cnn_labels_data_path = path.join(config["data_directory"],
+                                     config["cnn_y_data"])
+
+    np.save(cnn_features_data_path, segments, allow_pickle=True)
+    np.save(cnn_labels_data_path, labels, allow_pickle=True)
