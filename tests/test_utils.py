1f18ea2b780e1d96ef23b4201e80871f2de90bb8
cksajil <sajilck@gmail.com>
2023-02-09 05:59:05 +0530

    Added test code to sliding window function  On branch master  Changes to be committed: 	modified:   tests/test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
 from utils.general import load_config, selectModel
+from utils.general import strided_app
+import numpy as np
 
 
 def test_selectModel():
@@ -7,3 +9,10 @@ def test_selectModel():
 
 def test_load_config():
     assert type(load_config("my_config.yaml")) == dict
+
+
+def test_strided_app():
+    a = np.array((range(100)))
+    L = 20
+    S = 3
+    assert strided_app(a, L, S).shape == (27, 20)
