ab38578154b5e399adefdc9dee751fe28c4e40a1
cksajil <sajilck@gmail.com>
2023-02-22 16:47:44 +0530

    black styling dataProcessing.py

--- dataProcessing.py
+++ dataProcessing.py
@@ -6,18 +6,13 @@ from utils.general import load_config, generateDNNData, generateCNNData
 def main():
 
     config = load_config("my_config.yaml")
-    basicdata = pd.read_csv(path.join(config["data_directory"],
-                            config["basic_data"]))
+    basicdata = pd.read_csv(path.join(config["data_directory"], config["basic_data"]))
 
-    if config['default_model'] == 'myDNN':
-        generateDNNData(basicdata,
-                        frameS=config['FRAMES'],
-                        offset=config['OFFSET'])
+    if config["default_model"] == "myDNN":
+        generateDNNData(basicdata, frameS=config["FRAMES"], offset=config["OFFSET"])
     else:
-        generateCNNData(basicdata,
-                        frameS=config['FRAMES'],
-                        offset=config['OFFSET'])
+        generateCNNData(basicdata, frameS=config["FRAMES"], offset=config["OFFSET"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
